66e4dcd6e152f6f5d2fb6baa5ee743352ac50576
Wellington <welltecnic@hotmail.com>
2025-04-30 22:49:06 -0300

    adicoes

--- script.js
+++ script.js
@@ -17,10 +17,10 @@ imagem.setAttribute('src', 'images/avatar.jpg');
 imagem.alt = 'Avatar de Usuário';
 
 const caixa = document.querySelector('.box');
-// caixa.style.backgroundColor = 'lightgreen';
-// caixa.style.border = '10px solid black';
+caixa.style.backgroundColor = 'lightgreen';
+caixa.style.border = '10px solid black';
 
-// caixa.classList.add('oculta');
+caixa.classList.add('oculta');
 
 const botao = document.getElementById('meuBotao');
 botao.addEventListener('click', () => {
@@ -32,3 +32,24 @@ const novoItem = document.createElement('li');
 novoItem.textContent = 'Novo item';
 
 document.querySelector('ul').appendChild(novoItem);
+
+
+localStorage.setItem('nome', 'Daniel')
+
+const nome = localStorage.getItem('nome')
+
+console.log(nome)
+
+const tagNome = document.querySelector('#nome')
+
+tagNome.textContent = nome
+
+localStorage.removeItem('nome');
+
+const usuario = { nome: 'Lucas', idade: 25 };
+localStorage.setItem('usuario', JSON.stringify(usuario));
+
+const recuperado = localStorage.getItem('usuario');
+console.log(recuperado);
+console.log(JSON.parse(localStorage.getItem('usuario')));
+console.log(recuperado.nome);
